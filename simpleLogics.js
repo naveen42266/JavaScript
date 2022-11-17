5478744f302204e808719151f22c144be46f48c4
Naveen <99822733+naveen42266@users.noreply.github.com>
2022-11-17 22:59:43 +0530

    Create simpleLogics.js

---
+++ simpleLogics.js
@@ -0,0 +1,12 @@
+//swapping of 2 numbers
+
+var a=10;
+var b=5;
+a=a+b;
+b=a-b;
+a=a-b;
+console.log(a+" "+b); // it is one of the method to print
+a=a*b;
+b=a/b;
+a=a/b;
+alert(a+" "+b); // this is also one of the method to print
