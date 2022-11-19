c632aae8d871579f19d9b05ab2f0d104b034f958
Naveen <99822733+naveen42266@users.noreply.github.com>
2022-11-19 22:48:48 +0530

    Create countLettersAndSpecialCharacters.js

---
+++ countLettersAndSpecialCharacters.js
@@ -0,0 +1,9 @@
+var str="Hello developer !! Welcome to naveen42266";
+var count=0;
+for(var i=0;i<str.length;i++){
+    var char=str[i];
+    if(char !=" "){
+    count++;
+    }
+}
+console.log(count);
